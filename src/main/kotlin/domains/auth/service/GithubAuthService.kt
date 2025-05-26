e222b40fcd6d43a772537c9f439b725e310cce44
chobkyu <qudqud97@naver.com>
2025-05-26 23:34:45 +0900

    feat: OAuth handler

--- src/main/kotlin/domains/auth/service/GithubAuthService.kt
+++ src/main/kotlin/domains/auth/service/GithubAuthService.kt
@@ -1,25 +1,85 @@
 package org.example.domains.auth.service
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import okhttp3.FormBody
 import org.example.common.exceptions.CustomException
 import org.example.common.exceptions.Errcode
+import org.example.common.httpClient.CallClient
+import org.example.common.json.JsonUtil
 import org.example.config.OAuth2Config
 import org.example.interfaces.OAuth2TokenResponse
 import org.example.interfaces.OAuth2UserResponse
 import org.example.interfaces.OAuthServiceInterface
 import org.springframework.stereotype.Service
 
-private const val key = "google"
+private const val key = "github"
 
 @Service(key)
-class GithubAuthService (private val config: OAuth2Config): OAuthServiceInterface{
+class GithubAuthService (
+        private val config: OAuth2Config,
+        private val httpClient: CallClient
+    ): OAuthServiceInterface{
     private val oAuthInfo = config.providers[key] ?: throw CustomException(Errcode.AUTH_CONFIG_NOT_FOUND, key)
+
+    private val tokenURL = "https://github.com/login/oauth/access_token"
+    private val userInfoURL = "https://api.github.com/user"
     override val providerName: String = key
 
     override fun getToken(code: String): OAuth2TokenResponse {
-        TODO("Not yet implemented")
+        val body = FormBody.Builder()
+            .add("code",code)
+            .add("client_id",oAuthInfo.clientId)
+            .add("client_secret",oAuthInfo.clientSecret)
+            .add("redirect_uri", oAuthInfo.redirectUrl)
+            .add("grant_type", "authorization_code")
+            .build()
+
+        val headers = mapOf("Accept" to "application/json")
+
+        val jsonString = httpClient.POST(tokenURL,headers, body)
+
+        val response: GithubTokenResponse = JsonUtil.decodeFromJson(jsonString, GithubTokenResponse.serializer())
+
+        return response;
     }
 
+
     override fun getUserInfo(accessToken: String): OAuth2UserResponse {
-        TODO("Not yet implemented")
+        val headers = mapOf(
+            "Content-Type" to "application/json",
+            "Authorization" to "Bearer $accessToken"
+        )
+
+        val jsonString = httpClient.GET(userInfoURL,headers)
+        val response : GithubUserResponseTemp = JsonUtil.decodeFromJson(jsonString, GithubUserResponseTemp.serializer())
+
+        return response.toOAuth2UserResponse()
+
     }
 }
+
+@Serializable
+data class GithubTokenResponse(
+    @SerialName("access_token") override val accessToken: String
+): OAuth2TokenResponse
+
+@Serializable
+data class GithubUserResponseTemp(
+    val id: Int,
+    val repos_url: String,
+    val name: String
+) {
+    fun toOAuth2UserResponse() = GithubUserResponse(
+        id = id.toString(),
+        email = repos_url,
+        name = name
+    )
+}
+
+@Serializable
+data class GithubUserResponse(
+    override val id: String,
+    override val email: String?,
+    override val name: String?
+): OAuth2UserResponse
--- src/main/kotlin/domains/auth/service/GoogleAuthService.kt
+++ src/main/kotlin/domains/auth/service/GoogleAuthService.kt
@@ -1,7 +1,12 @@
 package org.example.domains.auth.service
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import okhttp3.FormBody
 import org.example.common.exceptions.CustomException
 import org.example.common.exceptions.Errcode
+import org.example.common.httpClient.CallClient
+import org.example.common.json.JsonUtil
 import org.example.config.OAuth2Config
 import org.example.interfaces.OAuth2TokenResponse
 import org.example.interfaces.OAuth2UserResponse
@@ -11,16 +16,55 @@ import org.springframework.stereotype.Service
 private const val key = "google"
 
 @Service(key)
-class GoogleAuthService (private val config: OAuth2Config): OAuthServiceInterface{
+class GoogleAuthService (
+        private val config: OAuth2Config,
+        private val httpClient: CallClient
+    ): OAuthServiceInterface{
 
     private val oAuthInfo = config.providers[key] ?: throw CustomException(Errcode.AUTH_CONFIG_NOT_FOUND,key)
+    private val tokenURL = "https://oauth2.googleapis.com/token"
+    private val userInfoURL = "https://www.googleapis.com/oauth2/v2/userinfo"
     override val providerName: String = key
 
     override fun getToken(code: String): OAuth2TokenResponse {
-        TODO("Not yet implemented")
+        val body = FormBody.Builder()
+            .add("code",code)
+            .add("client_id",oAuthInfo.clientId)
+            .add("client_secret",oAuthInfo.clientSecret)
+            .add("redirect_uri", oAuthInfo.redirectUrl)
+            .add("grant_type", "authorization_code")
+            .build()
+
+        val headers = mapOf("Accept" to "application/json")
+
+        val jsonString = httpClient.POST(tokenURL,headers, body)
+        val response: GoogleTokenResponse = JsonUtil.decodeFromJson(jsonString, GoogleTokenResponse.serializer())
+
+        return response;
+
     }
 
     override fun getUserInfo(accessToken: String): OAuth2UserResponse {
-        TODO("Not yet implemented")
+        val headers = mapOf(
+            "Content-Type" to "application/json",
+            "Authorization" to "Bearer $accessToken"
+        )
+
+        val jsonString = httpClient.GET(userInfoURL,headers)
+        val response : GoogleUserResponse = JsonUtil.decodeFromJson(jsonString, GoogleUserResponse.serializer())
+
+        return response
     }
 }
+
+@Serializable
+data class GoogleTokenResponse(
+    @SerialName("access_token") override val accessToken: String
+): OAuth2TokenResponse
+
+@Serializable
+data class GoogleUserResponse(
+    override val id: String,
+    override val email: String?,
+    override val name: String?
+): OAuth2UserResponse
