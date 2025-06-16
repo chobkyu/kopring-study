30b490e6afcc7340fafc251109a93587c0dd4bf8
chobkyu <qudqud97@naver.com>
2025-06-17 01:11:51 +0900

    bank setting

---
+++ src/main/kotlin/domains/bank/service/BankService.kt
@@ -0,0 +1,32 @@
+package org.example.domains.bank.service
+
+import org.example.common.logging.Logging
+import org.example.common.transaction.Transactional
+import org.springframework.stereotype.Service
+import org.slf4j.*
+
+@Service
+class BankService (
+    private val transaction: Transactional,
+    private val logger: Logger = Logging.getLogger(BankService::class.java)
+){
+    fun createAccount(userUlid: String) =Logging.logFor(logger){log->
+        log["userUlid"] = userUlid;
+
+        transaction.run {}
+    }
+
+    fun balance(userUlid: String, accountUlid: String)=Logging.logFor(logger) {log->
+        log["ulid"] = userUlid;
+        log["accountUlid"] = accountUlid;
+
+        transaction.run {}
+    }
+
+    fun removeAccount(userUlid: String, accountUlid: String) =Logging.logFor(logger) {log ->
+        log["ulid"] = userUlid;
+        log["accountUlid"] = accountUlid;
+
+        transaction.run {}
+    }
+}
